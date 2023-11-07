d45bceae52774dac041d70a9c17e8972889aabc7
Vitor Sofiati <vitor.sofiati@live.com>
2023-11-07 17:30:16 -0300

    Adicionados parâmetros URL para orçamento (JavaScript)

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// ativar links menu
+const links = document.querySelectorAll(".header-menu a");
+console.log(links);
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar items orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
+
